4d64d52f90ba85e25be08f5ef1c4d466eee63eec
FEsoftwareseni <nasser@softwareseni.com>
2016-09-26 15:38:22 +0700

    Plugin for toggle show and hide navbar when page scrolled

--- build/toggleNavBar.js
+++ build/toggleNavBar.js
@@ -9,16 +9,13 @@ https://github.com/firstpersoncode
   9/26/2016
   Very light Jquery Plugin for hide and show navbar on scrolling page.
 
-  
-  $('#navbar').autoScroll();
-
-  setting :
-  default
-  $('#navbar').autoScroll({
-	speed 		: 600,	||	scroll speed default '600' or 600ms (data type = number)
-	offset 		: 0,	||	offset position '10' will push offset 10px from its offset, '- 10' will pull offset 10px from its offset (data type = number)
-	callback 	: null	||	callback after reaching the destination (data type = function)
+  Invoke plugin
+  $('#navbar').toggleNav({
+	     slideEffect : true, // Toggle effect, (boolean). Only accept true or false.
+      speed : 300 // Toggle animation speed (number) limit of speed is 5000.
   });
+
+  the navbar will be positioned 'fixed' and stick on the top of screen with full width.
 
   
 ========================================================================= */
@@ -28,16 +25,17 @@ https://github.com/firstpersoncode
 
     //default setting
     var settings = $.extend({
-      fadeEffect : null,
-      slideEffect : null,
-      speed : 600,
-      callback : null
+      //Only one effect can be set, if two effects are true, the plugin will throw error
+      fadeEffect : null, // Toggle fadeIn fadeOut effect
+      slideEffect : null, // Toggle slide to top effect
+      speed : 600 // Toggle animation speed
     },togglenav);
 
     var slideTrue = settings.slideEffect == true,
           fadeTrue = settings.fadeEffect == true;    
 
     return this.each(function(){
+      //check all the settings are good to go !
       if(slideTrue&&fadeTrue){
         console.error('error, Can\'t set two effects at the same time');
         return false
@@ -50,13 +48,14 @@ https://github.com/firstpersoncode
         return false
       }
 
+      //handle the navbar
       $(this).addClass("toggleNavbar");
       
       var togglethis = jQuery('.toggleNavbar'),
-          previousScroll = 0,
-          headerHeight = togglethis.outerHeight();
+          previousScroll = 0,//record last scroll
+          headerHeight = togglethis.outerHeight();//record 'this' height
 
-      togglethis.css({
+      togglethis.css({//default styling for navbar
         position : 'fixed',
         top : '0',left : '0',
         width : '100%',
@@ -64,7 +63,7 @@ https://github.com/firstpersoncode
         'z-index' : 99998
       });
 
-      if(slideTrue){                
+      if(slideTrue){//if slide effect, then 'this' needs transition.                
         togglethis.css({
           WebkitTransition  : 'ease-out '+settings.speed/1000+'s',
           MozTransition     : 'ease-out '+settings.speed/1000+'s',
@@ -73,13 +72,8 @@ https://github.com/firstpersoncode
           transition        : 'ease-out '+settings.speed/1000+'s'
         });
       }
-      
-      function callback(){
-        if ( $.isFunction( settings.callback ) ) {//callback when the element is loaded
-          settings.callback.call( this );
-        }
-      }
-      function hideNav(){
+
+      function hideNav(){//handle toggle hide navbar
         if(fadeTrue){togglethis.fadeOut(settings.speed)}
         else if(slideTrue){          
           togglethis.css({
@@ -87,7 +81,7 @@ https://github.com/firstpersoncode
           });
         }
       }
-      function showNav(){
+      function showNav(){//handle toggle show navbar
         if(fadeTrue){togglethis.fadeIn(settings.speed)}
         else if(slideTrue){          
           togglethis.css({
@@ -96,18 +90,19 @@ https://github.com/firstpersoncode
         }
       }
 
+      //toggle navbar
       $(window).scroll(function(){
         var currentScroll = $(this).scrollTop();        
-          if (currentScroll > previousScroll) {
+          if (currentScroll > previousScroll) {//check if page scrolled to bottom
               hideNav();
-          } else {
+          } else { //check if page scrolled to top
               showNav();
           }
         previousScroll = currentScroll;
       });
     });
   }
-}(jQuery));
+}(jQuery));//end plugin
 
 /* =========================================================================
 
